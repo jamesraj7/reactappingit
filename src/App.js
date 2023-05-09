12e7c9e113f7339e87fc6ab76252e4ba42d7393a
JamesRaj <james@swiftant.com>
2023-05-09 15:59:23 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Hello React!
+          <h1>Hello React! js
           </h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
